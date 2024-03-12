35f6f5e7f9cfc36c31ca39fd46adc99f30801a6e
pablo <111470186+pablosanch423@users.noreply.github.com>
2024-03-12 12:54:54 -0700

    Update question4.js

--- question4.js
+++ question4.js
@@ -1,5 +1,5 @@
 const FizzBuzz = () => {
-  for (let i = 0; i <= 99; i++) {
+  for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("FizzBuzz");
     } else if (i % 3 === 0) {
